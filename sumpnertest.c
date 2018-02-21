bf2965e4d9272e477bd3a9cffb18183574ad82de
vivek6429 <vivek6429@gmail.com>
2018-02-21 03:54:16 -0800

    Update sumpnertest.c

--- sumpnertest.c
+++ sumpnertest.c
@@ -2,13 +2,13 @@
 #include<math.h>
 
 void effifunction(float X,float PI,float PC,float PF,float KVA);
-void regufunction(float X,float ISC,float RSC,float PF,float XSC,float VSC);
+void regufunction(float X,float ISC,float RSC,float PF,float XSC,float V02);
 
 void main()
 { // float voc=230.0,ioc=0.74,woc=146,roc,xoc,cosphi,sinphi,effi,pi,pc;
   // float vsc=148,isc=6.8,wsc=440,zsc,xsc,rsc,kva=3000.0;
   int i=0,j=0;
-  float voc,ioc,woc,roc,xoc,cosphi,sinphi,effi,pi,pc,iw,im;//for oc test
+  float voc,ioc,woc,roc,xoc,cosphi,sinphi,effi,pi,pc,iw,im,v02;//for oc test
   float vsc,isc,wsc,zsc,xsc,rsc,kva=3000.0;//for sc test
   float pf[5]={1.0,0.8,0.6,0.4,0.2};
   float load[4]={1.0,0.75,0.5,0.25};
@@ -21,6 +21,8 @@ void main()
   scanf( "%f %f %f",&voc,&ioc,&woc);
   printf("enter  vsc isc wsc :"); 
   scanf("%f %f %f",&vsc,&isc,&wsc);
+  printf("Enter v02 of transformer:");
+  scanf("%f",&v02);
 
  //////////calculations/////////////////
   woc/=2.0;ioc/=2.0;  //for single transformer
@@ -49,7 +51,7 @@ void main()
             for(j=0;j<5;j++)
              {printf("\n \t PF=%f",pf[j]);
 	       effifunction(load[i],pi,pc,pf[j],kva);
-	       regufunction(load[i],isc,rsc,pf[j],xsc,vsc);
+	       regufunction(load[i],isc,rsc,pf[j],xsc,v02);
              }
           }
          printf("\n\n\nRoc=%fΩ Xoc=%fΩ ",roc,xoc);
@@ -69,13 +71,13 @@ void effifunction(float X,float PI,float PC,float PF,float KVA)
    
 }
 ///////////////////////////////////////////////////////////////////////////
-void regufunction(float X,float ISC,float RSC,float PF,float XSC,float VSC)
+void regufunction(float X,float ISC,float RSC,float PF,float XSC,float V02)
 {	float regu; //lagging
-   	regu=X*100.0*((ISC*RSC*PF)+(ISC*XSC*sin(acos(PF))))/VSC;
+   	regu=X*100.0*((ISC*RSC*PF)+(ISC*XSC*sin(acos(PF))))/V02;
         if(PF!=1.0)
         {	printf("\nRegulation=%f (lagging )",regu);
         	//leading
-		regu=X*100.0*((ISC*RSC*PF)-(ISC*XSC*sin(acos(PF))))/VSC;
+		regu=X*100.0*((ISC*RSC*PF)-(ISC*XSC*sin(acos(PF))))/V02;
         	printf("\t Regulation=%f (leading)\n\n ",regu);
         } else 
 		{
